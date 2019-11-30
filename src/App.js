97a5b1e2778a30bbe04e63c5402a1d0ae1757eaf
Denilson Rocha <denilsonsrocha@gmail.com>
2019-11-29 23:46:36 -0300

    ajustes no tabuleiro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
@@ -7,15 +7,50 @@ import './App.css';
 
 function App() {
 
-  //const limpaTabuleiro = Array(9).fill("");
+  const limpaTabuleiro = Array(9).fill("");
 
-  const limpaTabuleiro = [
-    "O", "X", "X",
-    "O", "X", "X",
-    "O", "X", "X",
-  ]
+  // const limpaTabuleiro = [
+  //   "O", "X", "X",
+  //   "O", "X", "X",
+  //   "O", "X", "X",
+  // ]
 
   const [tabuleiro, setTabuleiro] = useState(limpaTabuleiro);
+  const [jogadorAtual, setJogadorAtual] = useState("O");
+  const [ganhador, setGanhador] = useState();
+
+  const handlerCampoClick = (index) => {
+
+    if(tabuleiro[index] !== "") return null;
+
+    setTabuleiro(tabuleiro.map((item, itemIndex) => itemIndex === index ? jogadorAtual : item));
+  
+    setJogadorAtual(jogadorAtual === "X"? "O" : "X")
+  }
+
+  
+
+  const verificaGanhador = () => {
+    const possibilidades = [
+      [tabuleiro[0],tabuleiro[1],tabuleiro[2]],
+      [tabuleiro[3],tabuleiro[4],tabuleiro[5]],
+      [tabuleiro[6],tabuleiro[7],tabuleiro[8]],
+
+      [tabuleiro[0],tabuleiro[3],tabuleiro[6]],
+      [tabuleiro[1],tabuleiro[4],tabuleiro[7]],
+      [tabuleiro[2],tabuleiro[5],tabuleiro[8]],
+
+      [tabuleiro[0],tabuleiro[4],tabuleiro[8]],
+      [tabuleiro[2],tabuleiro[4],tabuleiro[2]],
+    ]
+
+    possibilidades.forEach(campos => {
+      if(campos.every(campo => campo === "0")) setGanhador('O');
+      if(campos.every(campo => campo === "X")) setGanhador('X');
+    })
+  }
+
+  useEffect(verificaGanhador, [tabuleiro]);
 
   return (
     <main>
@@ -24,10 +59,15 @@ function App() {
       <div className="tabuleiro">
         {
           tabuleiro.map((item, index) => (
-            <div key={index} className={`campo ${item}`}>{item}</div>
+            <div 
+            key={index} 
+            className={`campo ${item}`}
+            onClick={() => handlerCampoClick(index)}
+            >
+              {item}
+            </div>
           ))
         }
-        <div className="campo">.</div>
       </div>
     </main>
   )
