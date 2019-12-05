dd03ff74737bbd988856bc90fff2d328c2d80870
denilsonsrocha <denilsonsrocha@gmail.com>
2019-12-05 10:13:55 -0300

    variavel de ambiente

--- src/pages/Jogo/index.js
+++ src/pages/Jogo/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const io = require('socket.io-client');
-const socket = io('http://localhost:3333');
+const socket = io(process.env.REACT_APP_API_URL);
 
 const Jogo = () => {
 
